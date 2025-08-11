9b87744f881a7a6f3fb3ce110cbf9ce915cec66a
eizw <hamzahamri2005@gmail.com>
2025-08-11 14:44:58 +0800

    changed Enter key behavior on login page, now only logs in when pressing added logo

--- frontend/src/lib/Login.svelte
+++ frontend/src/lib/Login.svelte
@@ -14,7 +14,7 @@
                 username,
                 password,
                 passwordConfirm: password,
-                name: 'hi mom!'
+                name: ''
             }
 
             const createdUser = await pb.collection('users').create(data);
@@ -28,23 +28,43 @@
     function signOut() {
         pb.authStore.clear();
     }
+
+    let body = document.querySelector('body');
+    body?.addEventListener('keydown', async (e) => {
+        if (e.key =='Enter') {
+            e.preventDefault();
+            await login();
+        }
+    })
 </script>
 
 {#if $currentUser}
     <p>Singed in as {$currentUser.username}</p>
 {:else}
-    <form on:submit|preventDefault>
+    <form on:submit|preventDefault
+        class="flex flex-col gap-3"
+    >
+        <div class="flex justify-center">
+            <img src="logo.png" alt="hasmil" class="max-w-52">
+        </div>
         <input
             placeholder="Username"
             type="text"
+            class="focus:ring-blue-500 focus:border-blue-500 
+            block w-full p-2.5 border rounded-lg border-orange-500 placeholder-gray-400 
+            text-white px-5 py-3"
             bind:value={username}
         >
         <input
             placeholder="Password"
             type="password"
+            class="focus:ring-blue-500 focus:border-blue-500 
+            block w-full p-2.5 border rounded-lg border-orange-500 placeholder-gray-400 
+            text-white px-5 py-3"
             bind:value={password}
         >
-        <button on:click={signUp}>Sign Up</button>
-        <button on:click={login}>Login</button>
+        <button on:click={signUp}
+        >Sign Up</button>
+        <button on:click={login} type="submit">Login</button>
     </form>
 {/if}
