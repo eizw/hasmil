2a335fcc8a845b8935e8e463336014d796ac5f97
eizw <hamzahamri2005@gmail.com>
2026-01-29 16:59:32 +0800

    Mid-refactoring

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,9 +1,82 @@
 <script lang="ts">
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+    import { currentUser, pb } from '../lib/pocketbase'
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
+
+<!-- Include this script tag or install `@tailwindplus/elements` via npm: -->
+<!-- <script src="https://cdn.jsdelivr.net/npm/@tailwindplus/elements@1" type="module"></script> -->
+<nav class="relative bg-gray-800 dark:bg-gray-800/50 dark:after:pointer-events-none dark:after:absolute dark:after:inset-x-0 dark:after:bottom-0 dark:after:h-px dark:after:bg-white/10 w-full">
+	<div class="mx-auto w-full px-2">
+	  <div class="relative flex h-16 items-center justify-between">
+		<div class="absolute inset-y-0 left-0 flex items-center">
+		  <!-- Mobile menu button-->
+		  <button type="button" command="--toggle" commandfor="mobile-menu" class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+			<span class="absolute -inset-0.5"></span>
+			<span class="sr-only">Open main menu</span>
+			<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 in-aria-expanded:hidden">
+			  <path d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" stroke-linecap="round" stroke-linejoin="round" />
+			</svg>
+			<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6 not-in-aria-expanded:hidden">
+			  <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />
+			</svg>
+		  </button>
+		</div>
+		<div class="flex flex-1 items-center justify-center">
+		  <div class="flex shrink-0 items-center">
+			<img src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" class="h-8 w-auto" />
+		  </div>
+		  <div class="hidden">
+			<div class="flex space-x-4">
+			  <!-- Current: "bg-gray-900 dark:bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
+			  <a href="#" aria-current="page" class="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white dark:bg-gray-950/50">Dashboard</a>
+			  <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Team</a>
+			  <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Projects</a>
+			  <a href="#" class="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Calendar</a>
+			</div>
+		  </div>
+		</div>
+		<div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+		  <button type="button" class="relative rounded-full p-1 text-gray-400 focus:outline-2 focus:outline-offset-2 focus:outline-indigo-500 dark:hover:text-white">
+			<span class="absolute -inset-1.5"></span>
+			<span class="sr-only">View notifications</span>
+			<svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" class="size-6">
+			  <path d="M14.857 17.082a23.848 23.848 0 0 0 5.454-1.31A8.967 8.967 0 0 1 18 9.75V9A6 6 0 0 0 6 9v.75a8.967 8.967 0 0 1-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 0 1-5.714 0m5.714 0a3 3 0 1 1-5.714 0" stroke-linecap="round" stroke-linejoin="round" />
+			</svg>
+		  </button>
+  
+		  <!-- Profile dropdown -->
+		  <el-dropdown class="relative ml-3">
+			<button class="relative flex rounded-full focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500">
+			  <span class="absolute -inset-1.5"></span>
+			  <span class="sr-only">Open user menu</span>
+			  <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" class="size-8 rounded-full bg-gray-800 outline -outline-offset-1 outline-white/10" />
+			</button>
+  
+			<el-menu anchor="bottom end" popover class="w-48 origin-top-right rounded-md bg-white py-1 shadow-lg outline outline-black/5 transition transition-discrete [--anchor-gap:--spacing(2)] data-closed:scale-95 data-closed:transform data-closed:opacity-0 data-enter:duration-100 data-enter:ease-out data-leave:duration-75 data-leave:ease-in dark:bg-gray-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+			  <a href="#" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden dark:text-gray-300 dark:focus:bg-white/5">Your profile</a>
+			  <a href="#" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden dark:text-gray-300 dark:focus:bg-white/5">Settings</a>
+			  <a href="#" class="block px-4 py-2 text-sm text-gray-700 focus:bg-gray-100 focus:outline-hidden dark:text-gray-300 dark:focus:bg-white/5">Sign out</a>
+			</el-menu>
+		  </el-dropdown>
+		</div>
+	  </div>
+	</div>
+  
+	<el-disclosure id="mobile-menu" hidden class="block md:hidden">
+	  <div class="space-y-1 px-2 pt-2 pb-3">
+		<!-- Current: "bg-gray-900 dark:bg-gray-950/50 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
+		<a href="#" aria-current="page" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white dark:bg-gray-950/50">Dashboard</a>
+		<a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Team</a>
+		<a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Projects</a>
+		<a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-white/5 hover:text-white">Calendar</a>
+	  </div>
+	</el-disclosure>
+  </nav>
+  
+
 {@render children()}
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,151 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    import { onMount, onDestroy } from 'svelte'
+    import { currentUser, pb } from '../lib/pocketbase'
+
+    // states
+    let showCreateTreeModal = false;
+
+    let trees: any[] = [];
+
+    let newTreeName: string = '';
+    
+
+    onMount(async () => {
+        // Get user's trees
+        const resultList = await pb.collection('tree').getList(1, 5, {
+            sort: 'created',
+            expand: 'user',
+            filter: `user.id = "${$currentUser!.id}"`,
+        })
+
+        trees = resultList.items
+
+        pb
+            .collection('tree')
+            .subscribe('*', async ({ action, record }) => {
+                if (record.user === $currentUser!.id) {
+                    if (action === 'create') {
+                        trees = [...trees, record];
+                    }
+                    if (action === 'delete') {
+                        trees = trees.filter((t) => t.id !== record.id);
+                    }
+                }
+            });
+        
+            
+
+        let body = document.querySelector('body');
+        body?.addEventListener('keydown', async (e) => {
+            if (e.key =='Enter') {
+                e.preventDefault();
+                await createTree();
+            }
+        })
+    })
+
+    function toggleCreateTreeModal() {
+        showCreateTreeModal = !showCreateTreeModal;
+    }
+
+    async function createTree() {
+        const data = {
+            'user': $currentUser!.id,
+            'name': newTreeName,
+        }
+
+        const newTree = await pb.collection('tree').create(data);
+        newTreeName = '';
+        toggleCreateTreeModal();
+        
+    }
+</script>
+
+{#if $currentUser}
+<div class="flex flex-col h-screen">
+    <p class="mb-3">Signed in as {$currentUser?.username}</p>
+    
+    <div class="h-6/12 flex flex-col gap-3 overflow-y-auto w-full">
+        {#each trees as tree (tree.id)}
+            <a href="/tree/{tree.id}">
+                <div 
+                    class="h-24 border-2 border-gray-500 rounded-lg flex justify-center align-middle hover:bg-zinc-600 transition-all"
+    
+                >
+                    <div class="flex flex-col justify-center align-middle ">
+                        {tree.name}
+                    </div>
+                </div>
+            </a>
+        {/each}
+    </div>
+    
+    <div id="createTreeModal" tabindex="-1" aria-hidden="true" class="hidden modal "
+        class:hidden={!showCreateTreeModal}
+    >
+        <div class="modal-content">
+            <!-- Modal content -->
+            <div class="relative rounded-lg shadow-md">
+                <!-- Modal header -->
+                <div class="flex items-center justify-between px-1 py-2">
+                    <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+                        New Tree
+                    </h3>
+                    <button type="button" class="text-gray-400 bg-transparent hover:bg-zinc-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center"
+                        on:click={toggleCreateTreeModal}>
+                        <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                    </button>
+                </div>
+                <!-- Modal body -->
+                <div class="space-y-4 px-1 py-2">
+                    <form on:submit|preventDefault={createTree}
+                        class="flex flex-col gap-3"
+                    >
+                        <input
+                            placeholder="Name"
+                            type="text"
+                            class="focus:ring-blue-500 focus:border-blue-500 
+                            block w-full p-2.5 border rounded-lg border-orange-500 placeholder-gray-400 
+                            text-white px-5 py-3"
+                            bind:value={newTreeName}
+                        >
+                    </form>
+                    
+                </div>
+                <!-- Modal footer -->
+                <div class="flex items-center px-1 py-2">
+                    <button on:click={createTree} type="submit" class="text-white bg-orange-500 hover:bg-zinc-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">Create</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="flex justify-center align-middle">
+        <button
+            type="button"
+            class="bottom-5 right-5
+                hover:border-orange-500 hover:bg-gray-900 hover:text-orange-500
+                transition-all
+                bg-orange-500 rounded-full text-white
+                size-12 p-3
+                text-5xl text-center
+            "
+            on:click={toggleCreateTreeModal}
+            title=""
+        >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 640" fill="white">
+                <path d="M352 128C352 110.3 337.7 96 320 96C302.3 96 288 110.3 288 128L288 288L128 288C110.3 288 96 302.3 96 320C96 337.7 110.3 352 128 352L288 352L288 512C288 529.7 302.3 544 320 544C337.7 544 352 529.7 352 512L352 352L512 352C529.7 352 544 337.7 544 320C544 302.3 529.7 288 512 288L352 288L352 128z"/>
+            </svg>
+        </button>
+
+    </div>
+
+</div>
+{:else}
+<a href="/login">
+    
+</a>
+{/if}
